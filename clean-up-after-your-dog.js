42a285d40160b19d2608c87910c75426f69e5e62
Heitor Godau <heitorgodau@gmail.com>
2019-04-23 09:44:39 -0300

    Kata Solved

---
+++ clean-up-after-your-dog.js
@@ -0,0 +1,39 @@
+const crap = (x, bags, cap) => {
+  let msg = '';
+  const craps = [];
+  for (let i = 0; i < x.length; i += 1) {
+    for (let j = 0; j < x[i].length; j += 1) {
+      if (x[i][j] === 'D') return 'Dog!!';
+      if (x[i][j] === '@') craps.push(x[i][j]);
+    }
+  }
+  ( bags * cap < craps.length ) ? msg = 'Cr@p' : msg = 'Clean';
+  return msg;
+};
+
+console.log(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 2, 2), "Clean");
+console.log(crap([['_','_','_','_'], ['_','_','_','@'], ['_','_','@', '_']], 1, 1), "Cr@p");
+console.log(crap([['_','_'], ['_','@'], ['D','_']], 2, 2), "Dog!!");
+
+/*You have stumbled across the divine pleasure that is owning a dog and a garden. Now time to pick up all the cr@p! :D
+
+Given a 2D array to represent your garden, you must find and collect all of the dog cr@p - represented by '@'.
+
+You will also be given the number of bags you have access to (bags), and the capactity of a bag (cap). If there are no bags then you can't pick anything up, so you can ignore cap.
+
+You need to find out if you have enough capacity to collect all the cr@p and make your garden clean again.
+
+If you do, return 'Clean', else return 'Cr@p'.
+
+Watch out though - if your dog is out there ('D'), he gets very touchy about being watched. If he is there you need to return 'Dog!!'.
+
+For example:
+
+x=
+[[_,_,_,_,_,_]
+[_,_,_,_,@,_]
+[@,_,_,_,_,_]]
+
+bags = 2, cap = 2
+
+return --> 'Clean'*/
